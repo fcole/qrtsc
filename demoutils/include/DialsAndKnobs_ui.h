b1db0e2ec3aab177f06293b03056d076b94497ff
Forrester Cole <fcole@mit.edu>
2010-07-01 10:09:11 -0400

    Breaking out ui into separate cc file to help linker errors?

--- demoutils/include/DialsAndKnobs_ui.h
+++ demoutils/include/DialsAndKnobs_ui.h
@@ -10,28 +10,17 @@
 class FileNameLineEdit : public QLineEdit
 {
     Q_OBJECT
+
   public:
     FileNameLineEdit(const QString& name) : _name(name) {}
+
   signals:
     void updateFilename(const QString& name);
     
-    public slots:
-    void setFromBrowser()
-    {
-        QString chosen = QFileDialog::getOpenFileName(this, 
-                QString("Choose %1").arg(_name), "", "");
-        
-        if (!chosen.isNull())
-        {
-            setText( chosen );
-            emit updateFilename(chosen);
-        }
-    }
-    
-    void callUpdateFilename()
-    {
-        emit updateFilename(text());
-    }
+  public slots:
+    void setFromBrowser();
+    void callUpdateFilename();
+
   private:
     QString _name;
 };
@@ -44,37 +33,16 @@ class UpdatingTextEdit : public QTextEdit
     void sendText(const QString& name);
     
   public slots:
-    void callSendText()
-    {
-        emit sendText(toPlainText());
-    }
-    void updateText(const QString& value)
-    {
-        if (value != toPlainText())
-        {
-            setPlainText(value);
-        }
-    }
+    void callSendText();
+    void updateText(const QString& value);
 };
 
 class ArbitraryPrecisionSpinBox : public QDoubleSpinBox
 {
   public:
-    ArbitraryPrecisionSpinBox(QWidget* parent = 0) : 
-        QDoubleSpinBox(parent)
-    {
-        setDecimals(15);
-        setKeyboardTracking(false);
-    }
+    ArbitraryPrecisionSpinBox(QWidget* parent = 0);
 
-    virtual QString textFromValue( double value ) const
-    {
-        QString str = QDoubleSpinBox::textFromValue(value);
-        int last_non_zero = str.lastIndexOf(QRegExp("[^0]"));
-        if (str[last_non_zero] == QWidget::locale().decimalPoint())
-            last_non_zero--;
-        return str.left(last_non_zero+1);
-    }
+    virtual QString textFromValue( double value ) const;
 };
 
 
